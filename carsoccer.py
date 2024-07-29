01d90940924fe892979ae43a21f0a9847259b078
Andrew Ma <ama25@belmontschools.net>
2024-07-28 21:52:06 -0400

    adding strucutres

---
+++ carsoccer.py
@@ -0,0 +1,98 @@
+import pygame
+import pymunk
+import pymunk.pygame_util
+import math
+
+pygame.init()
+width, height = 1400, 600
+window = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Car game!")
+white = (255, 255, 255)
+green = (50, 168, 82)
+black = (0, 0, 0)
+red = (200, 0, 0)
+grey = (105, 105, 105)
+blue = (66, 185, 189)
+FONT = pygame.font.Font(None, 40)
+
+
+def calculate_distance(p1, p2):
+    return math.sqrt((p2[1] - p1[1]) ** 2 + (p2[0] - p1[0]) ** 2)
+
+
+def calculate_angle(p1, p2):
+    return math.atan2(p2[1] - p1[1], p2[0] - p1[0])
+
+
+def create_boundaries(space, width, height):
+    l = []
+    rects = [
+        [(width / 2, height - 10), (width, 20)],
+        [(width / 2, 10), (width, 20)],
+        [(80, 40), (20, height / 3)],  # LEFT WALL
+        [(80, height - 120), (20, height / 2)],
+        [(10, 130), (120, 20)],
+        [(10, height / 2 + 40), (120, 20)],
+        [(width - 80, 40), (20, height / 3)],  # RIGHT WALL
+        [(width - 80, height - 120), (20, height / 2)],
+        [
+            (width - 10, 130),
+            (120, 20),
+        ],
+        [
+            (width - 10, height / 2 + 40),
+            (120, 20),
+        ],
+        [
+            (0, 200),
+            (20, 300),
+        ],
+        [
+            (width, 200),
+            (20, 300),
+        ],
+    ]
+    for pos, size in rects:
+
+        body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        body.position = pos
+        shape = pymunk.Poly.create_box(body, size)
+        shape.elasticity = 0.4
+        shape.friction = 0.5
+        shape.pair_index = "wall"
+
+        space.add(body, shape)
+
+        l.append(shape)
+
+    return l[0]
+
+
+def play():
+
+    run = True
+    clock = pygame.time.Clock()
+    FPS = 60
+    dt = 1 / FPS
+    space = pymunk.Space()
+    draw_options = pymunk.pygame_util.DrawOptions(window)
+    space.gravity = (0, 600)
+    create_boundaries(space, width, height)
+    while run:
+
+        keys = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        space.step(dt)
+        clock.tick(FPS)
+        window.fill(blue)
+        space.debug_draw(draw_options)
+        pygame.display.update()
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    play()
